8f15f820bfe41d1d2b67d0ffbbca4e54c2b24672
Fernando Teixeira <fpcteixeira@sapo.pt>
2026-02-24 20:32:27 +0000

    aula 24.02.26

---
+++ aula 24.02.26.py
@@ -0,0 +1,38 @@
+nome = "Gonçalo"
+
+print(nome) # escreve na consola
+
+"""
+tipos de dados
+
+int -
+string - 
+Doublw -
+bool - 
+"""
+
+#   :typeHint
+idade2:int = 12
+nome:str = "Gonçalo"
+altura:float = 1.2
+aprovado:bool = True
+
+print("---------")
+print(idade2)
+idade2 = "Dez"
+
+print(idade2)
+
+print("---------")
+
+"""
+str(x) -> converte x para string
+int(x) -> converte x para int
+str(x) -> converte x para string
+"""
+
+print(nome2, idade2, altura2, aprovado2)
+print("nome: " + nome2 +
+      "idade :" + str(idade2) +
+      "altura " + str(altura2)
+      "aprovado"
