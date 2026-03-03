572a3d6ff98dcd49c636f61cbcdf70b112af9b27
Fernando Teixeira <fpcteixeira@sapo.pt>
2026-03-03 21:11:07 +0000

    aula 03.03.26

--- exercicios 3 e 4.py
+++ exercicios 3 e 4.py
@@ -7,19 +7,19 @@
 num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
 # Calcula e imprime a soma
-soma = int(num1 + num2)
+soma = (num1 + num2)
 print(f"A soma dos dois números é: {soma}")
 
 print("------------")
 # 4 notas bimestrais e mostre a média
-nota1 = int(input("Digite a nota do 1º bimestre: "))
-nota2 = int(input("Digite a nota do 2º bimestre: "))
-nota3 = int(input("Digite a nota do 3º bimestre: "))
-nota4 = int(input("Digite a nota do 4º bimestre: "))
+nota1 = float(input("nota 1: "))
+nota2 = float(input("nota 2: "))
+nota3 = float(input("nota 3: "))
+nota4 = float(input("nota 4: "))
 
 soma = nota1 + nota2 + nota3 + nota4
 media = soma / 4
 # a media é:_____
-print(f"a media é:", media)
+print(f"a media é {media:.2f}")
 
 print("------------")
--- exercicios 5 e 6.py
+++ exercicios 5 e 6.py
@@ -3,24 +3,24 @@
 6 Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 """
 # Pede o valor em metros
-metros = float(input("Digite o valor em metros: "))
+valor = float(input("valor em metros: "))
 
 # Converte para centímetros (1m = 100cm)
-centimetros = metros * 100
+cm = valor * 100
 
 # Exibe o resultado
-print(f"{metros} metros equivalem a {centimetros:.0f} centímetros.")
+print(f"{valor:.2f}m equivalem a {cm:.2f}cm")
 
 print("------------")
 import math
 
 # Pede o raio do círculo
-raio = float(input("Digite o raio do círculo: "))
+raio_circulo = float(input("raio do círculo: "))
 
 # Calcula a área usando a fórmula (pi * raio ao quadrado)
-area = math.pi * (raio ** 2)
+area = math.pi * raio_circulo ** 2
 
 # Exibe o resultado com duas casas decimais
-print(f"A área do círculo com raio {raio} é: {area:.2f}")
+print(f"A área do círculo com raio {raio_circulo} é: {area:.3f}")
 
 print("------------")
